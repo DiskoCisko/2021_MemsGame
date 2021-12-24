c47aa47c037490f309e2da1c62042668b785d6ef
DiskoCisko <pollandjon@gmail.com>
2021-12-24 20:37:18 +0300

    Add data for task_1

--- src/components/Tasks/Task_text.js
+++ src/components/Tasks/Task_text.js
@@ -4,6 +4,7 @@ import './task_text.css';
 const Task_text = ({task}) => {
 
     return <div className='text'>
+    <h3>{task.title}</h3>
         {task.img&&<img className='text__img' src={task.img}/>}
         <p>
             {task.task}
